8695bd1ee2ecb0dd622aaf71c88affcf2c2565bc
Collin Van Dyck <collin@ngrok.com>
2023-02-24 17:56:49 -0500

    wip

--- tuple_test.go
+++ tuple_test.go
@@ -6,12 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTupleIsPoint(t *testing.T) {
-	tup := tuple{4.3, -4.2, 3.1, 1.0}
-	require.Equal(t, tup.x, 4.3)
-	require.Equal(t, tup.y, -4.2)
-	require.Equal(t, tup.z, 3.1)
-	require.Equal(t, tup.w, 1.0)
-	require.True(t, tup.isPoint())
-	require.False(t, tup.isVector())
+func TestTuples(t *testing.T) {
+	t.Run("A tuple with w=0 is a point", func(t *testing.T) {
+		tup := tuple{4.3, -4.2, 3.1, 1.0}
+		require.Equal(t, tup.x, 4.3)
+		require.Equal(t, tup.y, -4.2)
+		require.Equal(t, tup.z, 3.1)
+		require.Equal(t, tup.w, 1.0)
+		require.True(t, tup.isPoint())
+		require.False(t, tup.isVector())
+	})
 }
