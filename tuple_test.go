7fcf5d87508e96e7718e6860ca268deca2e6f583
Collin Van Dyck <collin@ngrok.com>
2023-02-24 18:02:55 -0500

    wip

--- tuple_test.go
+++ tuple_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestTuples(t *testing.T) {
-	t.Run("A tuple with w=0 is a point", func(t *testing.T) {
+	t.Run("A tuple with w=1 is a point", func(t *testing.T) {
 		tup := tuple{4.3, -4.2, 3.1, 1.0}
 		require.Equal(t, tup.x, 4.3)
 		require.Equal(t, tup.y, -4.2)
@@ -16,14 +16,9 @@ func TestTuples(t *testing.T) {
 		require.True(t, tup.isPoint())
 		require.False(t, tup.isVector())
 	})
-	t.Run("A point is a point", func(t *testing.T) {
-		point := point(4.3, -4.2, 3.1)
-		require.Equal(t, point.x, 4.3)
-		require.Equal(t, point.y, -4.2)
-		require.Equal(t, point.z, 3.1)
-		require.Equal(t, point.w, 1.0)
-		require.True(t, point.isPoint())
-		require.False(t, point.isVector())
+	t.Run("A point creates a tuple with w=1", func(t *testing.T) {
+		point := point(4, -4, 3)
+		require.Equal(t, tuple{4, -4, 3, 1}, point)
 	})
 	t.Run("A tuple with w=0 is a vector", func(t *testing.T) {
 		tup := tuple{4.3, -4.2, 3.1, 0.0}
@@ -34,13 +29,8 @@ func TestTuples(t *testing.T) {
 		require.False(t, tup.isPoint())
 		require.True(t, tup.isVector())
 	})
-	t.Run("A vector is a vector", func(t *testing.T) {
-		vector := vector(4.3, -4.2, 3.1)
-		require.Equal(t, vector.x, 4.3)
-		require.Equal(t, vector.y, -4.2)
-		require.Equal(t, vector.z, 3.1)
-		require.Equal(t, vector.w, 0.0)
-		require.False(t, vector.isPoint())
-		require.True(t, vector.isVector())
+	t.Run("A vector creates a tuple with w=0", func(t *testing.T) {
+		vector := vector(4, -4, 3)
+		require.Equal(t, tuple{4, -4, 3, 0}, vector)
 	})
 }
