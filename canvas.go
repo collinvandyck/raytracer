bd266e96c078f16027480666df094e533c6f5092
Collin Van Dyck <collin@ngrok.com>
2023-02-24 23:42:02 -0500

    wip

--- canvas.go
+++ canvas.go
@@ -1,24 +1,24 @@
 package rt
 
 type Canvas struct {
-	color [][]Color
+	colors [][]Color
 }
 
 func NewCanvas(width int, height int) *Canvas {
 	if width <= 0 || height <= 0 {
 		panic("dimensions must be positive")
 	}
-	res := &Canvas{color: make([][]Color, height)}
-	for i := range res.color {
-		res.color[i] = make([]Color, width)
+	res := &Canvas{colors: make([][]Color, height)}
+	for i := range res.colors {
+		res.colors[i] = make([]Color, width)
 	}
 	return res
 }
 
 func (c *Canvas) Width() int {
-	return len(c.color[0])
+	return len(c.colors[0])
 }
 
 func (c *Canvas) Height() int {
-	return len(c.color)
+	return len(c.colors)
 }
