d1b282f3c75b94128d2ec06477e7af6a7242b9f2
Collin Van Dyck <collin@ngrok.com>
2023-02-24 21:02:06 -0500

    wip

--- tuple_test.go
+++ tuple_test.go
@@ -54,8 +54,8 @@ func TestTupleAdd(t *testing.T) {
 		p1 := newPoint(3, -2, 5)
 		v1 := newVector(-2, 3, 1)
 		re := newPoint(1, 1, 6)
-		require.Equal(t, re, p1.addVector(v1))
-		require.Equal(t, re, v1.addPoint(p1))
+		equalPoint(t, re, p1.addVector(v1))
+		equalPoint(t, re, v1.addPoint(p1))
 	})
 }
 
@@ -64,36 +64,36 @@ func TestTupleSubtract(t *testing.T) {
 		t1 := tuple(3, -2, 5, 1)
 		t2 := tuple(-2, 3, 1, 0)
 		re := tuple(5, -5, 4, 1)
-		require.Equal(t, re, t1.subtract(t2))
+		equalTuple(t, re, t1.subtract(t2))
 	})
 	t.Run("Subtracting two points is a vector", func(t *testing.T) {
 		p1 := newPoint(3, -2, 5)
 		p2 := newPoint(-2, 3, 1)
-		re := p1.subtractPoint(p2)
-		require.Equal(t, newVector(5, -5, 4), re)
+		re := newVector(5, -5, 4)
+		equalVector(t, re, p1.subtractPoint(p2))
 	})
 	t.Run("Subtracting two vectors is a vector", func(t *testing.T) {
 		v1 := newVector(3, -2, 5)
 		v2 := newVector(-2, 3, 1)
-		re := v1.subtractVector(v2)
-		require.Equal(t, newVector(5, -5, 4), re)
+		re := newVector(5, -5, 4)
+		equalVector(t, re, v1.subtractVector(v2))
 	})
 	t.Run("Subtracting a vector from a point is a point", func(t *testing.T) {
 		p1 := newPoint(3, 2, 1)
 		v1 := newVector(5, 6, 7)
 		re := newPoint(-2, -4, -6)
-		require.Equal(t, re, p1.subtractVector(v1))
+		equalPoint(t, re, p1.subtractVector(v1))
 	})
 	t.Run("Subtracing a vector from the zero vector", func(t *testing.T) {
 		v1 := newVector(0, 0, 0)
 		v2 := newVector(1, -2, -3)
 		re := newVector(-1, 2, 3)
-		require.Equal(t, re, v1.subtractVector(v2))
+		equalVector(t, re, v1.subtractVector(v2))
 	})
 	t.Run("Negating a vector", func(t *testing.T) {
 		v1 := newVector(1, -2, -3)
 		re := newVector(-1, 2, 3)
-		require.Equal(t, re, v1.negate())
+		equalVector(t, re, v1.negate())
 	})
 }
 
@@ -102,13 +102,13 @@ func TestTupleMultiply(t *testing.T) {
 		const factor float = 3.5
 		t1 := tuple(1, -2, 3, -4)
 		re := tuple(1*factor, -2*factor, 3*factor, -4*factor)
-		require.Equal(t, re, t1.multiply(factor))
+		equalTuple(t, re, t1.multiply(factor))
 	})
 	t.Run("Multiplying a tuple by a fraction", func(t *testing.T) {
 		const factor float = 0.5
 		t1 := tuple(1, -2, 3, -4)
 		re := tuple(1*factor, -2*factor, 3*factor, -4*factor)
-		require.Equal(t, re, t1.multiply(factor))
+		equalTuple(t, re, t1.multiply(factor))
 	})
 }
 
@@ -117,7 +117,7 @@ func TestTupleDivide(t *testing.T) {
 		const factor float = 2
 		t1 := tuple(1, -2, 3, -4)
 		re := tuple(1/factor, -2/factor, 3/factor, -4/factor)
-		require.Equal(t, re, t1.divide(factor))
+		equalTuple(t, re, t1.divide(factor))
 	})
 }
 
