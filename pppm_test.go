a2949e5afee787e83f8070621ae3a6da3634ca9f
Collin Van Dyck <collin@ngrok.com>
2023-02-25 00:26:26 -0500

    wip

--- pppm_test.go
+++ pppm_test.go
@@ -23,4 +23,26 @@ func TestPPM(t *testing.T) {
 		require.True(t, br.Scan())
 		require.EqualValues(t, "255", br.Text())
 	})
+	t.Run("Constructing the PPM pixel data", func(t *testing.T) {
+		cv := NewCanvas(5, 3)
+		c1 := NewColor(1.5, 0, 0)
+		c2 := NewColor(0, 0.5, 1)
+		c3 := NewColor(-0.5, 0, 1)
+
+		cv.WritePixel(0, 0, c1)
+		cv.WritePixel(2, 1, c2)
+		cv.WritePixel(4, 2, c3)
+
+		buf := new(bytes.Buffer)
+		err := writePPM(cv, buf)
+		require.NoError(t, err)
+
+		br := bufio.NewScanner(buf)
+		require.True(t, br.Scan())
+		require.EqualValues(t, "P3", br.Text())
+		require.True(t, br.Scan())
+		require.EqualValues(t, "5 3", br.Text())
+		require.True(t, br.Scan())
+		require.EqualValues(t, "255", br.Text())
+	})
 }
