1bc7e2c3970c63272dcba10b25e64eb49fe60489
Collin Van Dyck <collin@ngrok.com>
2023-02-25 11:43:06 -0500

    wip

--- matrix.go
+++ matrix.go
@@ -2,6 +2,18 @@ package rt
 
 type Matrix [][]float
 
+func NewMatrix(rows, cols int) Matrix {
+	m := make(Matrix, rows)
+	for i := range m {
+		m[i] = make([]float, cols)
+	}
+	return m
+}
+
+func NewMatrixFromTable(table string) Matrix {
+	return Matrix{}
+}
+
 func (m Matrix) Get(row int, column int) float {
 	return m[row][column]
 }
--- matrix_test.go
+++ matrix_test.go
@@ -16,4 +16,15 @@ func TestMatrix(t *testing.T) {
 		}
 		require.Equal(t, 1.0, m.Get(0, 0))
 	})
+	t.Run("Constructing and testing a 4x4 matrix from table", func(t *testing.T) {
+		m := NewMatrixFromTable(`
+		    +---------------------------+
+			| 1    | 2    | 3    | 4    |
+			| 5.5  | 6.5  | 7.5  | 8.5  |
+			| 9    | 10   | 11   | 12   |
+			| 13.5 | 14.5 | 15.5 | 16.5 |
+		    +---------------------------+
+		`)
+		require.Equal(t, 1.0, m.Get(0, 0))
+	})
 }
