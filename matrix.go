3a52315f49316f08497eb762fee2dc5047c4b4a2
Collin Van Dyck <collin@ngrok.com>
2023-02-25 10:28:28 -0500

    wip

--- matrix.go
+++ matrix.go
@@ -1,3 +1,7 @@
 package rt
 
 type Matrix [][]float
+
+func (m Matrix) Get(row int, column int) float {
+	return m[row][column]
+}
--- matrix_test.go
+++ matrix_test.go
@@ -1,7 +1,19 @@
 package rt
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestMatrix(t *testing.T) {
-
+	t.Run("Constructing and testing a 4x4 matrix", func(t *testing.T) {
+		m := Matrix{
+			[]float{1, 2, 3, 4},
+			[]float{5.5, 6.5, 7.5, 8.5},
+			[]float{9, 10, 11, 12},
+			[]float{13.5, 14.5, 15.5, 16.5},
+		}
+		require.Equal(t, 1.0, m.Get(0, 0))
+	})
 }
