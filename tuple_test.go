d04ae804c06b02d9fd16a2b1aa724f0603f0b0ec
Collin Van Dyck <collin@ngrok.com>
2023-02-24 19:36:54 -0500

    wip

--- tuple_test.go
+++ tuple_test.go
@@ -41,6 +41,14 @@ func TestTupleAdd(t *testing.T) {
 		a2 := tuple(-2, 3, 1, 0)
 		require.True(t, tuple(1, 1, 6, 1).Equal(a1.add(a2)))
 	})
+	t.Run("Adding point to a vector", func(t *testing.T) {
+		a1 := point(3, -2, 5)
+		a2 := vector(-2, 3, 1)
+		re := a1.add(a2)
+		require.False(t, re.isVector())
+		require.True(t, re.isPoint())
+		require.Equal(t, point(1, 1, 6), re)
+	})
 	t.Run("Adding two points", func(t *testing.T) {
 		a1 := point(3, -2, 5)
 		a2 := point(-2, 3, 1)
