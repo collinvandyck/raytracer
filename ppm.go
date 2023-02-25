fb84043c3a9e0ba0dc570829fa4e373acd91323c
Collin Van Dyck <collin@ngrok.com>
2023-02-25 00:18:55 -0500

    wip

--- ppm.go
+++ ppm.go
@@ -1,7 +1,37 @@
 package rt
 
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
 
 func writePPM(canvas Canvas, dst io.Writer) error {
-	return nil
+	bw := bufio.NewWriter(dst)
+	writer := ppmWriter{
+		canvas: canvas,
+		writer: bw,
+	}
+	return writer.write()
+}
+
+type ppmWriter struct {
+	canvas Canvas
+	writer *bufio.Writer
+}
+
+func (w ppmWriter) write() error {
+	return w.writeHeader()
+}
+
+func (w ppmWriter) writeHeader() error {
+	width, height := w.canvas.Width(), w.canvas.Height()
+	header := fmt.Sprintf("P3\n%d %d\n255", width, height)
+	err := w.writeString(header)
+	return err
+}
+
+func (w ppmWriter) writeString(v string) error {
+	_, err := w.writer.WriteString(v)
+	return err
 }
