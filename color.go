7d4c6f3dc9d1fca12a8cadfdb3bf1c19240dc8ba
Collin Van Dyck <collin@ngrok.com>
2023-02-24 23:08:30 -0500

    wip

--- color.go
+++ color.go
@@ -6,6 +6,18 @@ func NewColor(r, g, b float) Color {
 	return Color(tuple(r, g, b, 0))
 }
 
+func (c Color) Add(o Color) Color {
+	return c
+}
+
+func (c Color) Subtract(o Color) Color {
+	return c
+}
+
+func (c Color) MultiplyBy(val float) Color {
+	return c
+}
+
 func (c Color) Red() float {
 	return c.x
 }
