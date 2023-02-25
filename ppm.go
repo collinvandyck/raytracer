6b2dfe2b20d5c559bc9db146052e5b26546d6513
Collin Van Dyck <collin@ngrok.com>
2023-02-25 00:45:41 -0500

    wip

--- ppm.go
+++ ppm.go
@@ -9,6 +9,7 @@ func writePPM(canvas Canvas, dst io.Writer) error {
 	writer := ppmWriter{
 		canvas: canvas,
 		writer: dst,
+		max:    255,
 	}
 	return writer.write()
 }
@@ -16,35 +17,53 @@ func writePPM(canvas Canvas, dst io.Writer) error {
 type ppmWriter struct {
 	canvas Canvas
 	writer io.Writer
+	max    int
+	err    error
 }
 
-func (w ppmWriter) write() error {
-	err := w.writeHeader()
-	if err != nil {
-		return err
-	}
-	err = w.writeBody()
-	if err != nil {
-		return err
-	}
-	return nil
+func (w *ppmWriter) write() error {
+	w.writeHeader()
+	w.writeBody()
+	return w.err
 }
 
-func (w ppmWriter) writeHeader() error {
+func (w *ppmWriter) writeHeader() {
 	width, height := w.canvas.Width(), w.canvas.Height()
-	header := fmt.Sprintf("P3\n%d %d\n255\n", width, height)
-	err := w.writeString(header)
-	return err
+	header := fmt.Sprintf("P3\n%d %d\n%d\n", width, height, w.max)
+	w.writeString(header)
 }
 
-func (w ppmWriter) writeBody() error {
-	width, height := w.canvas.Width(), w.canvas.Height()
-	header := fmt.Sprintf("P3\n%d %d\n255", width, height)
-	err := w.writeString(header)
-	return err
+func (w *ppmWriter) writeBody() {
+	for ri := 0; ri < w.canvas.Height(); ri++ {
+		for ci := 0; ci < w.canvas.Width(); ci++ {
+			px := w.canvas.PixelAt(ri, ci)
+			r := w.scale(px.Red())
+			g := w.scale(px.Green())
+			b := w.scale(px.Blue())
+			w.writeString(fmt.Sprintf("%d %d %d", r, g, b))
+			if ci < w.canvas.Width()-1 {
+				w.writeString(" ")
+			}
+		}
+		w.writeString("\n")
+	}
 }
 
-func (w ppmWriter) writeString(v string) error {
+func (w *ppmWriter) writeString(v string) {
+	if w.err != nil {
+		return
+	}
 	_, err := w.writer.Write([]byte(v))
-	return err
+	w.err = err
+}
+
+func (w *ppmWriter) scale(val float) int {
+	v := int(val * float(w.max))
+	if v < 0 {
+		v = 0
+	}
+	if v > w.max {
+		v = w.max
+	}
+	return v
 }
