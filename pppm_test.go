61baf51c2cb153625aede054b416737a7a6b96ee
Collin Van Dyck <collin@ngrok.com>
2023-02-25 00:23:07 -0500

    wip

--- pppm_test.go
+++ pppm_test.go
@@ -18,5 +18,11 @@ func TestPPM(t *testing.T) {
 		br := bufio.NewScanner(buf)
 		require.True(t, br.Scan())
 		require.EqualValues(t, "P3", br.Text())
+		require.True(t, br.Scan())
+		require.EqualValues(t, "5 3", br.Text())
+		require.True(t, br.Scan())
+		require.EqualValues(t, "255", br.Text())
+
+		require.Empty(t, buf.Bytes())
 	})
 }
