0296aec51927c263d141fc59c300e21d387900b1
Collin Van Dyck <collin@ngrok.com>
2023-02-25 19:13:58 -0500

    wip

--- matrix_test.go
+++ matrix_test.go
@@ -9,12 +9,12 @@ import (
 func TestMatrix(t *testing.T) {
 	t.Run("Constructing and testing a 4x4 matrix from table", func(t *testing.T) {
 		m := NewMatrixFromTable(`
-            +---------------------------+
-            | 1    | 2    | 3    | 4    |
-            | 5.5  | 6.5  | 7.5  | 8.5  |
-            | 9    | 10   | 11   | 12   |
-            | 13.5 | 14.5 | 15.5 | 16.5 |
-            +---------------------------+
+			+---------------------------+
+			| 1    | 2    | 3    | 4    |
+			| 5.5  | 6.5  | 7.5  | 8.5  |
+			| 9    | 10   | 11   | 12   |
+			| 13.5 | 14.5 | 15.5 | 16.5 |
+			+---------------------------+
 		`)
 		require.Equal(t, 1.0, m.Get(0, 0))
 		require.Equal(t, 4.0, m.Get(0, 3))
