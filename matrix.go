5dd734c41c32e6ecc605382d4a7cdc443d929f1d
Collin Van Dyck <collin@ngrok.com>
2023-02-25 21:49:18 -0500

    wip

--- matrix.go
+++ matrix.go
@@ -87,8 +87,21 @@ func (m Matrix) Multiply(o Matrix) (res Matrix) {
 	res = NewMatrix(m.Rows(), m.Cols())
 	for ri := 0; ri < m.Rows(); ri++ {
 		for ci := 0; ci < m.Cols(); ci++ {
+
+			// calculate the dot product and store in val
 			var val float
+
+			// ri = the row index
+			// ci = the col index
+
+			for lhsi := 0; lhsi < m.Cols(); lhsi++ {
+				lhs := m.Get(ri, lhsi)
+				rhs := o.Get(lhsi, ci)
+				val += (lhs * rhs)
+			}
+
 			res[ri][ci] = val
+
 		}
 	}
 	return
