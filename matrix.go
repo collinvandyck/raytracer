cb3dcde7cc1f5a6a12b3da4ffea4bcb05937004f
Collin Van Dyck <collin@ngrok.com>
2023-02-26 00:03:39 -0500

    wip

--- matrix.go
+++ matrix.go
@@ -103,11 +103,12 @@ func (m Matrix) MultiplyTuple4(t1 Tuple4) (res Tuple4) {
 	}
 	vals := [4]float{}
 	for ri := 0; ri < m.Rows(); ri++ {
-		for lhsi := 0; lhsi < m.Cols(); lhsi++ {
-			lhs := m.Get(ri, lhsi)
-			//vals[ri] += val
-			_ = lhs
-		}
+		var val float
+		val += m.Get(ri, 0) * t1.x
+		val += m.Get(ri, 1) * t1.y
+		val += m.Get(ri, 2) * t1.z
+		val += m.Get(ri, 3) * t1.w
+		vals[ri] = val
 	}
 	res.x, res.y, res.z, res.w = vals[0], vals[1], vals[2], vals[3]
 	return
