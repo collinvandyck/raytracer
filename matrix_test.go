38b10121b24090fc1a63b729bfda8aeec6b00022
Collin Van Dyck <collin@ngrok.com>
2023-02-26 12:19:34 -0500

    wip

--- matrix_test.go
+++ matrix_test.go
@@ -185,6 +185,24 @@ func TestMatrix(t *testing.T) {
 			`)
 			require.EqualValues(t, 17, m1.Determinant())
 		})
+		t.Run("Calculating the determinant of a 3x3 matrix", func(t *testing.T) {
+			m1 := NewMatrixFromTable(`
+			+-------------------+
+			|  1.0 | 2.0 |  6.0 |
+			| -5.0 | 8.0 | -4.0 |
+			|  2.0 | 6.0 |  4.0 |
+			+-------------------+
+			`)
+			cf00 := m1.Cofactor(0, 0)
+			cf01 := m1.Cofactor(0, 1)
+			cf02 := m1.Cofactor(0, 2)
+			dt1 := m1.Determinant()
+
+			require.EqualValues(t, 56, cf00)
+			require.EqualValues(t, 12, cf01)
+			require.EqualValues(t, -46, cf02)
+			require.EqualValues(t, -196, dt1)
+		})
 	})
 	t.Run("Submatrixes", func(t *testing.T) {
 		t.Run("A submatrix of a 3x3 matrix is a 2x2 matrix", func(t *testing.T) {
