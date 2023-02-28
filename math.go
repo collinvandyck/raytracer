e9353aac5786f7e342821d34f79a580fb9b9cddb
Collin Van Dyck <collin@ngrok.com>
2023-02-27 23:44:07 -0500

    wip

--- math.go
+++ math.go
@@ -2,7 +2,10 @@ package rt
 
 import "math"
 
-const EPSILON = 0.00001
+const (
+	EPSILON = 0.00001
+	Pi      = math.Pi
+)
 
 func floatsEqual(v1, v2 float) bool {
 	return math.Abs(v1-v2) < EPSILON
