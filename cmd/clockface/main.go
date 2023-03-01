7e7e35373a9d3093d1c77fd5c2b7bb406fe44ea9
Collin Van Dyck <collin@ngrok.com>
2023-02-28 21:40:09 -0500

    wip

--- cmd/clockface/main.go
+++ cmd/clockface/main.go
@@ -11,20 +11,34 @@ func main() {
 	c := rt.NewCanvas(500, 500)
 	c.SetPointSize(4)
 
-	tCenter := rt.Translation(float64(c.Width())/2, float64(c.Height())/2, 0)
+	var (
+		cx = float64(c.Width()) / 2
+		cy = float64(c.Height()) / 2
+		tc = rt.Translation(cx, cy, 0) // shift (0,0) to center of canvas
+		ts = rt.Scaling(1, -1, 1)      // flip the y coord
+		t1 = tc.Multiply(ts)           // flip the y coord, and then shift to center
+	)
 
-	writePoint := func(point rt.Point) {
-		p1 := tCenter.MultiplyPoint(point)
+	// transforms and then writes the point to the canvas
+	write := func(point rt.Point) {
+		p1 := t1.MultiplyPoint(point)
 		c.WritePoint(p1, color)
 		fmt.Println("Writing point", p1)
 	}
 
 	origin := rt.NewPoint(0, 0, 0)
-	writePoint(origin)
+	write(origin)
 
-	// start at 12:00
-	start := rt.NewPoint(0, 180, 0)
-	writePoint(start)
+	// current at 12:00 (noon)
+	current := rt.NewPoint(0, 180, 0)
+	write(current)
+
+	// rotate by an hour (1/3 * pi/2) for the remaining hours
+	rotate := rt.RotationZ(rt.Pi / -6)
+	for i := 1; i < 12; i++ {
+		current = rotate.MultiplyPoint(current)
+		write(current)
+	}
 
 	err := rt.WritePPMTo(c, "clockface.ppm")
 	if err != nil {
