1e8a104fbc4de3a3036051e54b15061dc506e142
Collin Van Dyck <collin@ngrok.com>
2023-03-05 18:14:32 -0500

    wip

--- sphere_test.go
+++ sphere_test.go
@@ -4,4 +4,15 @@ import "testing"
 
 func TestSphere(t *testing.T) {
 
+	t.Run("A sphere's default transformation", func(t *testing.T) {
+		s1 := NewSphere()
+		equalMatrix(t, MatrixIdentity4x4, s1.GetTransform())
+	})
+
+	t.Run("Changing a sphere's transformation", func(t *testing.T) {
+		s1 := NewSphere()
+		t1 := Scaling(2, 2, 3)
+		s1.SetTransform(t1)
+		equalMatrix(t, t1, s1.GetTransform())
+	})
 }
