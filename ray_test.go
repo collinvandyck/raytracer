74e894f0c22493b14a29e52f9b54a846bcce56d0
Collin Van Dyck <collin@ngrok.com>
2023-03-05 16:37:24 -0500

    wip

--- ray_test.go
+++ ray_test.go
@@ -25,42 +25,4 @@ func TestRays(t *testing.T) {
 		equalPoint(t, NewPoint(4.5, 3, 4), r1.Position(2.5))
 	})
 
-	t.Run("A ray intersects a sphere at two points", func(t *testing.T) {
-		r1 := NewRay(NewPoint(0, 0, -5), NewVector(0, 0, 1))
-		s1 := NewSphere()
-		vs := IntersectSphere(s1, r1)
-		equalValueSlice(t, []value{4, 6}, vs)
-	})
-
-	t.Run("A ray intersects a sphere at a tangent", func(t *testing.T) {
-		r1 := NewRay(NewPoint(0, 1, -5), NewVector(0, 0, 1))
-		s1 := NewSphere()
-		vs := IntersectSphere(s1, r1)
-		equalValueSlice(t, []value{5, 5}, vs)
-	})
-
-	t.Run("A ray misses a sphere", func(t *testing.T) {
-		r1 := NewRay(NewPoint(0, 2, -5), NewVector(0, 0, 1))
-		s1 := NewSphere()
-		vs := IntersectSphere(s1, r1)
-		equalValueSlice(t, nil, vs)
-	})
-
-	t.Run("A ray originates inside a sphere", func(t *testing.T) {
-		r1 := NewRay(NewPoint(0, 0, 0), NewVector(0, 0, 1))
-		s1 := NewSphere()
-		vs := IntersectSphere(s1, r1)
-		equalValueSlice(t, []value{-1, 1}, vs)
-	})
-
-	t.Run("A sphere is behind a ray", func(t *testing.T) {
-		r1 := NewRay(NewPoint(0, 0, 5), NewVector(0, 0, 1))
-		s1 := NewSphere()
-		vs := IntersectSphere(s1, r1)
-		equalValueSlice(t, []value{-6, -4}, vs)
-	})
-
-	t.Run("An intersection encapsulates t and object", func(t *testing.T) {
-	})
-
 }
