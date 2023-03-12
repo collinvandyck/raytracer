8c3c204cc85d4c843e65a0d5729cfc05fc445aa1
Collin Van Dyck <collin@ngrok.com>
2023-03-12 09:33:37 -0400

    wip

--- transformations.go
+++ transformations.go
@@ -4,5 +4,27 @@ package rt
 // to  : where you want to look
 // up  : which direction is "up"
 func ViewTransform(from Point, to Point, up Vector) Matrix {
-	return MatrixIdentity4x4
+	var (
+		forward  = to.SubtractPoint(from)
+		normalUp = up.Normalize()
+		left     = forward.Cross(normalUp)
+		trueUp   = left.Cross(forward)
+	)
+
+	// orientation
+	o := NewMatrix(4, 4)
+	o.Set(0, 0, left.X())
+	o.Set(0, 1, left.Y())
+	o.Set(0, 2, left.Z())
+	o.Set(1, 0, trueUp.X())
+	o.Set(1, 1, trueUp.Y())
+	o.Set(1, 2, trueUp.Z())
+	o.Set(2, 0, -forward.X())
+	o.Set(2, 1, -forward.Y())
+	o.Set(2, 2, -forward.Z())
+	o.Set(3, 3, 1)
+
+	// translate to move the scene into place
+	res := o.Multiply(Translation(-from.X(), -from.Y(), -from.Z()))
+	return res
 }
