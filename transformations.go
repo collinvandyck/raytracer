15d0dec42f06ab7f2230b20d2894721d1736679f
Collin Van Dyck <collin@ngrok.com>
2023-03-11 22:14:13 -0500

    wip

--- transformations.go
+++ transformations.go
@@ -1,5 +1,8 @@
 package rt
 
+// from: where you want the eye to be in the scene
+// to  : where you want to look
+// up  : which direction is "up"
 func ViewTransform(from Point, to Point, up Vector) Matrix {
 	return MatrixIdentity4x4
 }
