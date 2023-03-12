439d536b208b21786e16a6e8f8050b1699f3c2f8
Collin Van Dyck <collin@ngrok.com>
2023-03-12 09:23:13 -0400

    wip

--- transformations_test.go
+++ transformations_test.go
@@ -36,4 +36,19 @@ func TestTransformations(t *testing.T) {
 		equalMatrix(t, Scaling(0, 0, -8), m1)
 	})
 
+	t.Run("An arbitrary view transformation", func(t *testing.T) {
+		from := NewPoint(1, 3, 2)
+		to := NewPoint(4, -2, 8)
+		up := NewVector(1, 1, 0)
+		m1 := ViewTransform(from, to, up)
+		me := NewMatrixFromTable(`
+			+------------------------------------------+
+			| -0.50709 | 0.50709 |  0.67612 | -2.36643 |
+			|  0.76772 | 0.60609 |  0.12122 | -2.82843 |
+			| -0.35857 | 0.59761 | -0.71714 |        0 |
+			|        0 |       0 |        0 |        1 |
+			+------------------------------------------+`)
+		equalMatrix(t, me, m1)
+	})
+
 }
